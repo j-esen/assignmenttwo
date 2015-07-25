6cb890c58110c859a896be2f0b37ef6865069ad7
j-esen <j.esen@outlook.com>
2015-07-25 15:08:56 -0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -54,9 +54,8 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
-			'database'  => 'take2',
-			'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+			'host'      => 'localhost',
+			'database'  => 'jesentwo',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
